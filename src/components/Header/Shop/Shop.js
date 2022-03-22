ce7e7349e84730732571ad5123fa4742c10d5da8
Eliyas Hossain <eliyashossain700@gmail.com>
2022-03-22 11:26:20 +0600

    Data loaded from Source

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
 const Shop = () => {
+    const [products,setProducts]=useState([])
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    }, [])
     return (
-        <div>
-            <h3>this Is shop</h3>
+        <div className='shop-container'>
+            <div className="products-container">
+                <h4>This is from Products Container {products.length}</h4>
+            </div>
+            <div className="cart-container">
+                <h4>This is Cart Container</h4>
+            </div>
         </div>
     );
 };
